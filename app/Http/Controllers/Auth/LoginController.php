1e5d4c9048b5486771100c282b78e2af058a7cae
Dulan Bandara <50689681+DulanNB@users.noreply.github.com>
2022-10-28 00:26:07 +0530

    nuxt auth

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -14,6 +14,7 @@ class LoginController extends Controller
     public function __invoke(Request $request){
         if(!auth()->attempt($request->only('email','password'))){
             throw new AuthenticationException();
+            //throw new AuthenticationException();
         }
     }
 }
